202def6b500e2464a3389d7c29713307b76e54eb
Emmanuel <ekasomo@corp.free.fr>
2024-02-09 21:30:00 +0100

    update code

--- mma/items.py
+++ mma/items.py
@@ -4,9 +4,28 @@
 # https://docs.scrapy.org/en/latest/topics/items.html
 
 import scrapy
+from scrapy import Item, Field
 
-
-class MmaItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class MmaItem(Item):
+    Name = scrapy.Field()
+    Weight = scrapy.Field()
+    Win = scrapy.Field()
+    Lose = scrapy.Field()
+    Draw = scrapy.Field()
+    DOB = scrapy.Field()
+    Height = scrapy.Field()
+    Reach = scrapy.Field()
+    Stance = scrapy.Field()
+    Significant_Strikes_Landed_per_Minute = scrapy.Field()
+    Significant_Striking_Accuracy = scrapy.Field()
+    Significant_Strikes_Absorbed_per_Minute = scrapy.Field()
+    Significant_Strike_Defence = scrapy.Field()
+    Average_Takedowns_Landed_per_15_minutes = scrapy.Field()
+    Takedown_Accuracy = scrapy.Field()
+    Takedown_Defense = scrapy.Field()
+    Average_Submissions_Attempted_per_15_minutes = scrapy.Field()
+    WL = scrapy.Field()
+    Fighters = scrapy.Field()
+    Events = scrapy.Field()
+    Record = Field()
+    Fighters_and_Events = Field()
--- mma/pipelines.py
+++ mma/pipelines.py
@@ -6,8 +6,34 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
-
+import json
 
 class MmaPipeline:
+    def open_spider(self, spider):
+        self.file = open('fighters.json', 'w')
+        self.data = []
+
+    def close_spider(self, spider):
+        self.file.write(json.dumps(self.data))
+        self.file.close()
+
     def process_item(self, item, spider):
+        self.data.append(dict(item))
+        return item
+
+class DataProcessingPipeline:
+    def process_item(self, item, spider):
+        win, lose, draw = item['Record'].split('-')
+        item['Win'] = win
+        item['Lose'] = lose
+        item['Draw'] = draw
+        del item['Record']
+
+        fighters_and_events = item['Fighters_and_Events']
+        fighters = [item.strip() for item in fighters_and_events if not any(event in item for event in ["UFC", "DWCS", "PRIDE","DREAM","Shooto","DEEP","Fury", "dynamite"])]
+        events = [item.strip() for item in fighters_and_events if any(event in item for event in ["UFC", "DWCS", "PRIDE","DREAM","Shooto","DEEP","Fury", "dynamite"])]
+        item['Fighters'] = fighters
+        item['Events'] = events
+        del item['Fighters_and_Events']
+
         return item
