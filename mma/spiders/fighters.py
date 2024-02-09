58cb4f2e102e7b7ce65b079cb98d2520509ad6bd
MoAlyOff <mohamedaly1899@gmail.com>
2024-02-09 14:54:31 +0100

    test

--- mma/spiders/fighters.py
+++ mma/spiders/fighters.py
@@ -1,74 +1,74 @@
 import scrapy
+import json
+from datetime import datetime
 
-
-class FightersSpider(scrapy.Spider):
-    name = "fighters"
+class FighterSpider(scrapy.Spider):
+    name = "fighter"
     allowed_domains = ["ufcstats.com"]
     start_urls = ["http://ufcstats.com/statistics/fighters"]
 
     def parse(self, response):
-        letters = response.css('body > section > div > div > div > div.b-statistics__nav-inner > div > ul > li.b-statistics__nav-item > a::text').extract()
+        # Extract and print each fighter's first name
+        fighter_links = response.css('td.b-statistics__table-col a::attr(href)').getall()
+        for link in fighter_links:
+            if link is not None:
+                yield response.follow(link, self.parse_fighter)
 
-        for letter in letters:
-            letter_url = f'http://ufcstats.com/statistics/fighters?char={letter.strip()}&page=all'
-            yield scrapy.Request(letter_url, callback=self.parse_letter)
-
-    def parse_letter(self, response):
-        fighter_links = response.css('tr.b-statistics__table-row td.b-statistics__table-col a::attr(href)').extract()
-
-        for fighter_link in fighter_links:
-            yield scrapy.Request(fighter_link, callback=self.parse_fighter)
-
-        upcoming_fight_links = response.css('tr.b-statistics__table-row td.b-statistics__table-col a::attr(href)').extract()
-
-        for upcoming_fight_link in upcoming_fight_links:
-            absolute_upcoming_fight_link = f"{response.url}{upcoming_fight_link}"
-            # yield scrapy.Request(absolute_upcoming_fight_link, callback=self.parse_upcoming_fight)
+        # Follow pagination links
+        next_pages = response.css('ul.b-statistics__paginate a::attr(href)').getall()
+        for next_page in next_pages:
+            if next_page is not None:
+                yield response.follow(next_page, self.parse)
 
     def parse_fighter(self, response):
-        fighter_name = response.css('span.b-content__title-highlight::text').get().strip()
+        # Extract data from the fighter's page
+        full_name = response.css('span.b-content__title-highlight::text').get().strip()
+        weight = response.css('li.b-list__box-list-item:contains("Weight:")::text').getall()[-1].strip()
+        record = response.css('span.b-content__title-record::text').get().strip().split(':')[1]
+        win, lose, draw = record.split('-')
+        dob = response.css('li.b-list__box-list-item:contains("DOB:")::text').getall()[-1].strip()
 
-        fighter_stats = {
-            'fighter_name': fighter_name,
-            'height': response.css('body > section > div > div > div.b-list__info-box.b-list__info-box_style_small-width.js-guide > ul > li:nth-child(1)::text').extract()[1].strip(),
-            'weight': response.css('body > section > div > div > div.b-list__info-box.b-list__info-box_style_small-width.js-guide > ul > li:nth-child(2)::text').extract()[1].strip(),
+        # Extract additional personal information
+        height = response.css('li.b-list__box-list-item:contains("Height:")::text').getall()[-1].strip()
+        reach = response.css('li.b-list__box-list-item:contains("Reach:")::text').getall()[-1].strip()
+        stance = response.css('li.b-list__box-list-item:contains("STANCE:")::text').getall()[-1].strip()
+
+        # Extract additional statistics
+        slpm = response.css('li.b-list__box-list-item:contains("SLpM:")::text').getall()[-1].strip()
+        str_acc = response.css('li.b-list__box-list-item:contains("Str. Acc.:")::text').getall()[-1].strip()
+        sapm = response.css('li.b-list__box-list-item:contains("SApM:")::text').getall()[-1].strip()
+        str_def = response.css('li.b-list__box-list-item:contains("Str. Def:")::text').getall()[-1].strip()
+        td_avg = response.css('li.b-list__box-list-item:contains("TD Avg.:")::text').getall()[-1].strip()
+        td_acc = response.css('li.b-list__box-list-item:contains("TD Acc.:")::text').getall()[-1].strip()
+        td_def = response.css('li.b-list__box-list-item:contains("TD Def.:")::text').getall()[-1].strip()
+        sub_avg = response.css('li.b-list__box-list-item:contains("Sub. Avg.:")::text').getall()[-1].strip()
+
+        # Extract W/L data from the table of matches
+        wl_data = response.css('tr.b-fight-details__table-row td.b-fight-details__table-col p.b-fight-details__table-text a.b-flag i.b-flag__inner i.b-flag__text::text').getall()
+
+        # Store the data in a dictionary
+        fighter_data = {
+            'Name': full_name,
+            'Weight': weight,
+            'Win': win,
+            'Lose': lose,
+            'Draw': draw,
+            'DOB': dob,
+            'Height': height,
+            'Reach': reach,
+            'Stance': stance,
+            'Significant Strikes Landed per Minute': slpm,
+            'Significant Striking Accuracy': str_acc,
+            'Significant Strikes Absorbed per Minute': sapm,
+            'Significant Strike Defence': str_def,
+            'Average Takedowns Landed per 15 minutes': td_avg,
+            'Takedown Accuracy': td_acc,
+            'Takedown Defense': td_def,
+            'Average Submissions Attempted per 15 minutes': sub_avg,
+            'W/L': wl_data
         }
 
-        print(fighter_stats)
-
-        previous_fight_links = response.css('tr.b-fight-details__table-row td.b-fight-details__table-col:nth-child(2) a::attr(href)').extract()
-
-        previous_fights = []
-
-        for previous_fight_link in previous_fight_links:
-            absolute_previous_fight_link = response.urljoin(previous_fight_link)
-            # yield scrapy.Request(absolute_previous_fight_link, callback=self.parse_previous_fight, meta={'previous_fights': previous_fights})
-
-        upcoming_fight_links = response.css('tr.b-fight-details__table-row td.b-fight-details__table-col:nth-child(3) a::attr(href)').extract()
-
-        upcoming_fights = []
-
-        for upcoming_fight_link in upcoming_fight_links:
-            absolute_upcoming_fight_link = response.urljoin(upcoming_fight_link)
-            # yield scrapy.Request(absolute_upcoming_fight_link, callback=self.parse_upcoming_fight, meta={'upcoming_fights': upcoming_fights})
-
-        # fighter_stats['previous_fights'] = response.meta['previous_fights']
-        # fighter_stats['upcoming_fights'] = response.meta['upcoming_fights']
-        # yield fighter_stats
-
-    def parse_previous_fight(self, response):
-        previous_fight_stats = {
-            'date': response.css('span.b-fight-details__text-date::text').get().strip(),
-            'result': response.css('div.b-fight-details__persons-side.i-b-fight-details__persons-side div:nth-child(1) i::text').get().strip(),
-        }
-
-        response.meta['previous_fights'].append(previous_fight_stats)
-        yield previous_fight_stats
-
-    def parse_upcoming_fight(self, response):
-        upcoming_fight_stats = {
-            'date': response.css('span.b-fight-details__text-date::text').get().strip(),
-        }
-
-        response.meta['upcoming_fights'].append(upcoming_fight_stats)
-        yield upcoming_fight_stats
+        # Write the dictionary to a JSON file
+        with open('fighters.json', 'a') as f:
+            json.dump(fighter_data, f)
+            f.write('\n')
