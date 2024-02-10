d37d4a1049e162e6d02fbe701d0d202f44eeda22
MoAlyOff <49441256+MoAlyOff@users.noreply.github.com>
2024-02-10 12:38:52 +0100

    Update settings.py

--- mma/settings.py
+++ mma/settings.py
@@ -64,6 +64,7 @@ ROBOTSTXT_OBEY = True
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 ITEM_PIPELINES = {
    "mma.pipelines.MmaPipeline": 300,
+   "mma.pipelines.DataProcessingPipeline":200,
 }
 
 # Enable and configure the AutoThrottle extension (disabled by default)
